1b8226bd040af0cf3067debdeb1b426fa40ae090
Robin Schroer <sulami@peerwire.org>
2015-09-18 00:38:20 +0200

    divide the data into chunks

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,6 @@ import           Lib
 main :: IO ()
 main = do
   filename <- fmap head getArgs
-  filedata <- lines <$> readFile filename
-  putStrLn . unlines $ take 15 filedata
+  filedata <- readFile filename
+  putStrLn . head $ chunks filedata
 
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,16 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import           Data.Char (isNumber)
+
+-- | Divide the blob of data into chunks that contain a single traceroute each.
+chunks :: String -> [String]
+chunks []   = []
+chunks blob = let l = lines blob
+                  (h,r) = splitAt 2 l
+                  (d,n) = span isData r
+              in (unlines h ++ unlines d) : chunks (unlines n)
+
+-- | Decide if a line is part of the header or data.
+isData :: String -> Bool
+isData = any isNumber . take 2
+
