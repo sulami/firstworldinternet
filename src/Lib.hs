99c1e69e2d9adf035e7a0c8f83fb862a40c65d95
Robin Schroer <sulami@peerwire.org>
2015-09-18 17:19:46 +0200

    add some time parsing

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,6 +2,19 @@ module Lib where
 
 import           Data.Char (isNumber)
 
+import           Data.Time.Clock (UTCTime)
+import           Data.Time.Format (defaultTimeLocale, parseTimeM)
+
+-- | Parse the inital time line of each chunk to an actual time. Because time
+-- does not know about CEST, use sed to replace all occurences of it with "B",
+-- which is the military designation.
+readTime :: String -> Maybe UTCTime
+readTime = parseTimeM True defaultTimeLocale "%e. %b %H:%M:%S %Z %Y" . prep
+  where
+    -- We need this to drop the German weekday prefix.
+    prep :: String -> String
+    prep = unwords . drop 1 . words
+
 -- | Divide the blob of data into chunks that contain a single traceroute each.
 chunks :: String -> [String]
 chunks []   = []
